1a97a63c6a72beb99927ca7ad1dceb7c19114b7b
Dale Smith <dale.smith.phd@gatech.edu>
2021-04-17 12:13:16 -0400

    Update build.sh
    add command-line argument processing and instructions to use

--- build.sh
+++ build.sh
@@ -2,9 +2,18 @@
 
 #
 # Build Docker image for cpuminer-opt and optionally push to DockerHub
+# See 
 #
 
 set -e
+
+usage_print() {
+	echo "$0 [-n | --no-cache] [-u | --username <username>] [-v | --version <version>] [-p | --push]"
+	echo "-n | --no-cache Optional. If present, pass --no-cache to docker build command."
+	echo "-u | --username <username> Optional. Username to be created for the container. Defaults to admin."
+	echo "-v | --version <version> Optional. cpuminer-opt version (see https://github.com/JayDDee/cpuminer-opt/releases). Omit the 'v'. Defaults to 3.16.2."
+	echo "-p | --push Optional. Push to DockerHub. Used by the author; requires 'docker login' with proper credentials."
+}
 
 no_cache=""
 version="3.16.2"
@@ -16,6 +25,11 @@ do
             no_cache="--no-cache"
             shift
             ;;
+        -u | --username)
+			shift
+			username=$1
+			shift
+			;;
         -v | --version)
             shift
             version=$1
@@ -26,6 +40,9 @@ do
             will_push=1
             shift
             ;;
+        -h | --help)
+            usage_print
+            exit 1
         *)
             echo "Unknown argument: $1"
             usage_print
@@ -34,9 +51,15 @@ do
     esac
 done
 
-docker build ${no_cache} --build-arg username=admin --build-arg cpuminer_opt_version=${version} --tag dtsmith2001/cpuminer-opt:v${version} -f cpuminer-opt.dockerfile .
+echo "Build Docker image dtsmith2001/cpuminer-opt:v${version} using version v${version}".
+
+docker build ${no_cache} --build-arg username=${username} --build-arg cpuminer_opt_version=${version} --tag dtsmith2001/cpuminer-opt:v${version} -f cpuminer-opt.dockerfile .
+
+echo "Build successful. Use 'docker run --rm -it dtsmith2001/cpuminer-opt:v${version}'"
 
 if [ ${will_push} -eq 1 ]
 then
+	echo "Push to dtsmith2001/cpuminer-opt:v${version}"
     docker push dtsmith2001/cpuminer-opt:v${version}
+    echo "Successfully pushed to DockerHub".
 fi
